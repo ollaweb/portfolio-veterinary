eaca4fd1500bf5d40d01dcba7aa01d9e02e97e0f
Olga L <olla.bradford@gmail.com>
2022-02-03 17:19:17 +0300

    Modal window with with choice of city + spoiler on the header. JS added

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@
 закрытие при переходе по ссылке меню)
 */
 
-// const body = document.querySelector("body")
+const body = document.querySelector("body")
 // const burger = document.querySelector(".burger");
 // const burgerItems = document.querySelector(".burger__items");
 // const menuAside = document.querySelector(".menu__items");
@@ -199,6 +199,107 @@ $(document).ready(function () {
         ]
     });
 });
+// ============== Header открытие спойлера ===============
+const spoiler = document.querySelector(".spoiler");
+const spoilerWrapper = document.querySelector(".spoiler__wrapper");
+const spoilerItem = document.querySelectorAll(".spoiler__item a");
+
+//Закрыьть блок спойлера
+function spoilerHide() {
+    spoilerWrapper.classList.remove("opened-spoiler");
+}
+
+//Переключение открыто/закрыто спойлера
+function spoilerToggle() {
+    spoilerWrapper.classList.toggle("opened-spoiler");
+}
+
+//Когда нажато "О нас" спойлер открывается, закрывается
+spoiler.addEventListener("click", () => {
+    spoilerToggle();
+});
+//Нажатие на пункт меню из блока спойлера закройдет блок спойлера
+spoilerWrapper.addEventListener("click", (event) => {
+    console.log(event.target);
+    spoilerItem.forEach(element => {
+        if (event.target == element) {
+            spoilerHide();
+        }
+    });
+});
 
 // ============== Header навигация по городам ===============
-const city = document.querySelector(".header__location");
+const headerLocation = document.querySelector(".header__location");
+const headerCity = document.querySelector(".header__city");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+const modalCity = document.querySelectorAll(".modal__city");
+const modalClose = document.querySelector(".modal__close");
+const allCities = document.querySelectorAll(".contacts__wrapper");
+const volgograd = document.querySelector(".contacts__volgograd");
+const grozny = document.querySelector(".contacts__grozny");
+
+//Открыть модальное окно
+function modalOpen() {
+    overlay.classList.add("overlay_opened");
+}
+
+//Скрыть модальное окно
+function modalHide() {
+    overlay.classList.remove("overlay_opened");
+    body.classList.toggle("_lock");
+}
+
+//При клике на локацию в шапке сайта откроется модальное окно
+headerLocation.addEventListener("click", (event) => {
+    modalOpen();
+    body.classList.toggle("_lock");
+});
+
+//При клике на крестик модального окна, модальное окно скроется
+modalClose.addEventListener("click", () => {
+    modalHide();
+});
+
+//При клике на затемненную подложку модальное окно скроется
+overlay.addEventListener("click", (event) => {
+    if (event.target == overlay) {
+        modalHide();
+    }
+});
+
+//Работа при клике в модальном окне
+modal.addEventListener("click", (event) => {
+    //Для каждого города, что есть в списке модального окна
+    modalCity.forEach(element => {
+        //Если мы нажали на любой город в списке мордального окна
+        if (event.target == element) {
+            //Название города записать в переменную cityName
+            const cityName = element.innerHTML;
+            //Если название выбранного города совпадает с тем,
+            //что уже стоит в шапке сайта,
+            //то ничего не происходит
+            if (cityName == headerCity.innerHTML) {
+                //Иначе, если мы выбрали город "Грозный"
+            } else if (cityName == "Грозный") {
+                //Скрыть все блоки в секции "Контакты"
+                allCities.forEach(city => {
+                    city.classList.remove("opened-city");
+                });
+                //Вывести блок для города "Грозный"
+                grozny.classList.add("opened-city");
+                //Аналогично для другого города
+            } else if (cityName == "Волгоград") {
+                allCities.forEach(city => {
+                    city.classList.remove("opened-city");
+                });
+                volgograd.classList.add("opened-city");
+            }
+            //Вписать выбранное название города из списка
+            //модального окна в шапку сайта
+            headerCity.textContent = cityName;
+            //Скрыть модальное окно
+            modalHide();
+        }
+    });
+});
