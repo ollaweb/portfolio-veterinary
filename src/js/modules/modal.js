f19a3e08f6f8bdafdc12759e262a778d837e2b28
Olga L <olla.bradford@gmail.com>
2022-02-15 18:47:18 +0300

    Modal windows appear! I did it!

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,42 +1,45 @@
 'use strict'
 function modal() {
-    function useModal(btnModal, classModal) {
+    function applyModalWindowToButton(btnModal, classModal) {
         const modalBtn = document.querySelector(btnModal);
         const modalClass = document.querySelector(classModal);
-        const modalClose = document.querySelector(classModal).querySelector(".modal__close");
-        const modalOverlay = document.querySelector(classModal).querySelector(".overlay");
-        const modalSubmit = document.querySelector(".modal__button");
-        modalBtn.addEventListener("click", (event) => {
-            if (event.target) {
+
+        if (modalBtn) {
+            const modalClose = modalClass.querySelector(".modal__close");
+            const modalOverlay = modalClass.querySelector(".overlay");
+            const modalButtons = document.querySelectorAll(".modal__button");
+
+            modalBtn.addEventListener("click", (event) => {
+                if (event.target) {
+                    event.preventDefault();
+                }
+                modalClass.style.display = "block";
+                document.body.style.overflow = "hidden";
+            });
+            modalClose.addEventListener("click", () => {
+                modalClass.style.display = "";
+                document.body.style.overflow = "";
+            });
+            modalClass.addEventListener("click", (event) => {
                 event.preventDefault();
-            }
-            modalClass.style.display = "block";
-            document.body.style.overflow = "hidden";
-        });
-        modalClose.addEventListener("click", () => {
-            modalClass.style.display = "";
-            document.body.style.overflow = "";
-        });
-        modalClass.addEventListener("click", (event) => {
-            console.log(event.target);
-            if (event.target === modalOverlay) {
-
-                modalClass.style.display = "";
-                document.body.style.overflow = "";
-            } else if (event.target === modalSubmit) {
-                // event.preventDefault();
-                modalClass.style.display = "";
-                document.body.style.overflow = "";
-            }
-        });
-
+                if (event.target === modalOverlay) {
+                    modalClass.style.display = "";
+                    document.body.style.overflow = "";
+                }
+                modalButtons.forEach(button => {
+                    if (event.target === button) {
+                        modalClass.style.display = "";
+                        document.body.style.overflow = "";
+                    }
+                });
+            });
+        }
     }
 
+    applyModalWindowToButton(".header__location", ".modal-location");
+    applyModalWindowToButton("#login", ".modal-login");
+    applyModalWindowToButton(".aside__feedback", ".modal-feedback");
+    applyModalWindowToButton(".modal__button_feedback", ".modal-thanks");
 
-
-    useModal(".header__location", ".modal-location");
-    useModal("#login", ".modal-login");
-    useModal(".aside__feedback", ".modal-feedback");
-    useModal(".modal-feedback .modal__button", ".modal-thanks");
 }
 export default modal;
